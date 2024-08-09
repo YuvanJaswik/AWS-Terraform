cc7a78f503a2db3e3eebb851ab1db00f5b33df8f
YuvanJaswik <arepallyrajavardhanreddy@gmail.com>
2024-08-10 01:05:12 +0530

    Create variables.tf

---
+++ variables.tf
@@ -0,0 +1,9 @@
+variable "ACCESS_KEY" {
+  description = "AKIAQ3EGV4SNDWAULXF5"
+  sensitive = true
+}
+
+variable "SECRET_KEY" {
+  description = "9R//uhYP/KjsXugnItQBTJQhOFc5IszN/1Zm0zQu"
+  sensitive = true
+}
