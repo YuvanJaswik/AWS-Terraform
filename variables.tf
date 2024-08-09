418887e61b40a6204904037835e903a011e9c33f
YuvanJaswik <arepallyrajavardhanreddy@gmail.com>
2024-08-10 01:20:33 +0530

    Delete variables.tf

--- variables.tf
+++
@@ -1,9 +0,0 @@
-variable "ACCESS_KEY" {
-  description = "AKIAQ3EGV4SNDWAULXF5"
-  sensitive = true
-}
-
-variable "SECRET_KEY" {
-  description = "9R//uhYP/KjsXugnItQBTJQhOFc5IszN/1Zm0zQu"
-  sensitive = true
-}
